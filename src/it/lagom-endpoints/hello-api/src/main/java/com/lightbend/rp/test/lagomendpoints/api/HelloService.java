4b4b2d87543bc644a7e14ab3f640961d9b4bc41a
Andrius Mitkus <andrius.mitkus@gmail.com>
2018-04-19 11:03:26 +0100

    Fix lagom test, add classpath discovery for LagomApp

--- src/it/lagom-endpoints/hello-api/src/main/java/com/lightbend/rp/test/lagomendpoints/api/HelloService.java
+++ src/it/lagom-endpoints/hello-api/src/main/java/com/lightbend/rp/test/lagomendpoints/api/HelloService.java
@@ -1,6 +1,6 @@
 package com.lightbend.rp.test.lagomendpoints.api;
 
-import static com.lightbend.lagom.javadsl.api.Service;
+import static com.lightbend.lagom.javadsl.api.Service.*;
 
 import akka.NotUsed;
 import com.lightbend.lagom.javadsl.api.Descriptor;
@@ -8,12 +8,12 @@ import com.lightbend.lagom.javadsl.api.Service;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
 
 public interface HelloService extends Service {
-    ServiceCall<NotUsed, String> hello();
+    ServiceCall<NotUsed, String> hello(String id);
 
     @Override
     default Descriptor descriptor() {
         // @formatter:off
-        return names("hello").withCalls(
+        return named("hello").withCalls(
                 pathCall("/api/hello/:id", this::hello)
         ).withAutoAcl(true);
         // @formatter:off
