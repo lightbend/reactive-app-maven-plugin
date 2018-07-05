18d9d68e55f3aec909f44c43205984a2a0ddba52
Dale Wijnand <dale.wijnand@gmail.com>
2018-07-05 18:04:38 +0100

    Update plugin version in IT projects using xmlstarlet

--- bin/run-integration-tests.sh
+++ bin/run-integration-tests.sh
@@ -15,6 +15,12 @@ it_test() {
 
   (
     cd "$dir" || die "Failed to cd into $dir"
+
+    find . -name 'pom.xml' -exec \
+      xmlstarlet ed -S --inplace -N pom=http://maven.apache.org/POM/4.0.0 \
+      --update "/pom:project/pom:build/pom:plugins/pom:plugin[.//pom:artifactId='reactive-app-maven-plugin']/pom:version" \
+      -v "$project_version" {} +
+
     mvn clean install
 
     echo "Generating K8s resources for $docker_image and applying with kubectl"
@@ -24,6 +30,8 @@ it_test() {
 }
 
 # prerequisite: minikube start & eval $(minikube docker-env)
+# prerequisite: brew install xmlstarlet
+project_version=$(xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -m "/pom:project/pom:version" -v '.' pom.xml) || true
 mvn install
 it_test src/it/hello           hello:1.0-SNAPSHOT
 it_test src/it/akka-quickstart akka-quickstart:1.0
