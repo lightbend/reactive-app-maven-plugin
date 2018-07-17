dd2cfc879959608d59fdc13a0828734f4349d759
Andrius Mitkus <andrius.mitkus@gmail.com>
2018-07-17 16:22:45 +0300

    Fix integration tests, make test-runner do kubectl dry-runs to make it faster

--- bin/run-integration-tests.sh
+++ bin/run-integration-tests.sh
@@ -26,7 +26,7 @@ it_test() {
     mvn clean install
 
     echo "Generating K8s resources for $docker_image and applying with kubectl"
-    rp generate-kubernetes-resources --generate-all "$docker_image" | kubectl apply -f - \
+    rp generate-kubernetes-resources --generate-all --registry-use-local "$docker_image" | kubectl apply --validate --dry-run -f - \
       || die "Failed to generate & apply k8s resources for $docker_image"
   )
 }
@@ -36,8 +36,8 @@ it_test() {
 # TODO: Handle potential xmlstarlet failure
 project_version=$(xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -m "/pom:project/pom:version" -v '.' pom.xml)
 mvn install
-it_test src/it/hello           hello:1.0-SNAPSHOT
+it_test src/it/hello           hello:1.0
 it_test src/it/akka-quickstart akka-quickstart:1.0
-#it_test src/it/play-endpoints  play-endpoints:1.0
-#it_test src/it/lagom-endpoints lagom-endpoints:1.0
+it_test src/it/play-endpoints  play-endpoints:1.0
+it_test src/it/lagom-endpoints hello-impl:1.0
 it_test src/it/akka-cluster    akka-cluster:1.0
