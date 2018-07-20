116653aac2e03fbc6209441c5ce791d22e36d735
Eugene Yokota <eed3si9n@gmail.com>
2018-07-20 00:08:55 -0400

    Add surefire to hello test

--- src/it/hello/src/main/java/App.java
+++ src/it/hello/src/main/java/App.java
@@ -4,10 +4,12 @@ package com.lightbend.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    public static String greeting = "Hello, world!";
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(greeting);
     }
 }
