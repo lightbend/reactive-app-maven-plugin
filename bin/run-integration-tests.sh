17afa0340efae3e97b9f784de6cd74c9eac6d9b4
Dale Wijnand <dale.wijnand@gmail.com>
2018-07-24 16:19:57 +0100

    Disable gpg, as well as tests & javadoc in integration tests

--- bin/run-integration-tests.sh
+++ bin/run-integration-tests.sh
@@ -16,7 +16,7 @@ it_test() {
   (
     cd "$dir" || die "Failed to cd into $dir"
 
-    mvn clean install
+    mvn -DskipTests=true -Dmaven.javadoc.skip=true -Dgpg.skip=true clean install
 
     echo "Generating K8s resources for $docker_image and applying with kubectl"
     rp generate-kubernetes-resources --generate-all --registry-use-local "$docker_image" | kubectl apply --validate --dry-run -f - \
@@ -34,7 +34,7 @@ it_test() {
 }
 
 # prerequisite: minikube start & eval $(minikube docker-env)
-mvn install
+mvn -DskipTests=true -Dmaven.javadoc.skip=true -Dgpg.skip=true install
 bin/sync-integration-tests-plugin-version.sh || die "Failed to sync integration tests plugin version"
 it_test src/it/hello           hello:1.0
 it_test src/it/akka-quickstart akka-quickstart:1.0
