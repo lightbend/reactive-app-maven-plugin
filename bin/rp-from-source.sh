725f143c64df6b2988b155e761407cb6c7677527
Dale Wijnand <dale.wijnand@gmail.com>
2018-07-27 12:14:10 +0100

    Fix clone detection in rp-from-source

--- bin/rp-from-source.sh
+++ bin/rp-from-source.sh
@@ -8,6 +8,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 dir="$DIR/reactive-cli"
 
-[[ -d reactive-cli ]] || git clone --depth 1 --branch v1.3.0 https://github.com/lightbend/reactive-cli "$dir"
+[[ -d "$dir" ]] || git clone --depth 1 --branch v1.3.0 https://github.com/lightbend/reactive-cli "$dir"
 cd "$dir" || die "Failed to cd into reactive-cli"
 sbt "cliNative/run $*"
