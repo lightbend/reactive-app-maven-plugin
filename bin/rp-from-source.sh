a8d9711e376a2a8e83522beb7b9ae4e9ecf14c53
Dale Wijnand <dale.wijnand@gmail.com>
2018-07-27 12:14:25 +0100

    Make rp-from-source ignore most sbt output..

--- bin/rp-from-source.sh
+++ bin/rp-from-source.sh
@@ -7,7 +7,14 @@ die() { echo "Aborting: $*"; exit 1; }
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 dir="$DIR/reactive-cli"
-
 [[ -d "$dir" ]] || git clone --depth 1 --branch v1.3.0 https://github.com/lightbend/reactive-cli "$dir"
 cd "$dir" || die "Failed to cd into reactive-cli"
-sbt "cliNative/run $*"
+
+export BINTRAY_USER=dummy
+export BINTRAY_PASS=dummy
+
+sbt -Dsbt.log.noformat=true "cliNative/run $*" < /dev/null 2>&1 \
+  | grep -v "\[success\]" \
+  | grep -v "\[error\]" \
+  | grep -v "\[info\]" \
+  | grep -v "\[warn\]"
